a72ee027200241b7667179b77078c9e73881bf1b
lajafar <142849075+lajafar@users.noreply.github.com>
2023-09-29 13:59:04 -0400

    fixing JS attempt2

--- index.js
+++ index.js
@@ -16,20 +16,23 @@ const meterToFeet =  3.281
 const literToGallon =  0.264
 const kiloToPound =  2.204
 
-convertBtn.addEventListener("click", function() {
-    let baseValue = Number(inputEl.value);
+function convertBtn() {
+    convertBtn.addEventListener("click", function() {
+        let baseValue = Number(inputEl.value);
+    
+        // Calculate the converted values and round them to 3 decimal places
+        const feetValue = (baseValue * meterToFeet).toFixed(3);
+        const metersValue = (baseValue / meterToFeet).toFixed(3);
+        const gallonsValue = (baseValue * literToGallon).toFixed(3);
+        const litersValue = (baseValue / literToGallon).toFixed(3);
+        const poundsValue = (baseValue * kiloToPound).toFixed(3);
+        const kilosValue = (baseValue / kiloToPound).toFixed(3);
+    
+        lengthEl.textContent = `${baseValue} meters = ${feetValue} feet | ${baseValue} feet = ${metersValue} meters`;
+        volumeEl.textContent = `${baseValue} liters = ${gallonsValue} gallons | ${baseValue} gallons = ${litersValue} liters`;
+        massEl.textContent = `${baseValue} kilos = ${poundsValue} pounds | ${baseValue} pounds = ${kilosValue} kilos`;
+    });
+}
 
-    // Calculate the converted values and round them to 3 decimal places
-    const feetValue = (baseValue * meterToFeet).toFixed(3);
-    const metersValue = (baseValue / meterToFeet).toFixed(3);
-    const gallonsValue = (baseValue * literToGallon).toFixed(3);
-    const litersValue = (baseValue / literToGallon).toFixed(3);
-    const poundsValue = (baseValue * kiloToPound).toFixed(3);
-    const kilosValue = (baseValue / kiloToPound).toFixed(3);
-
-    lengthEl.textContent = `${baseValue} meters = ${feetValue} feet | ${baseValue} feet = ${metersValue} meters`;
-    volumeEl.textContent = `${baseValue} liters = ${gallonsValue} gallons | ${baseValue} gallons = ${litersValue} liters`;
-    massEl.textContent = `${baseValue} kilos = ${poundsValue} pounds | ${baseValue} pounds = ${kilosValue} kilos`;
-});
 
 window.convertBtn = convertBtn
